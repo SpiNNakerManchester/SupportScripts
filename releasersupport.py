2f3eb3abfda55d6349cf36b8611e9292bba7d31d
alan-stokes <alan.barry.stokes@gmail.com>
2018-10-03 15:59:43 +0100

    adds the scripts to spinnutils instead of support scripts and adds the funcitonality for bibtexing the citation file.
    needs testing the codes again, esp the doi stuff

---
+++ releasersupport.py
@@ -0,0 +1,13 @@
+
+
+
+class ReleaserSupport(object):
+
+    def __init__(self):
+        self._modules_to_release = dict()
+        self._modules_to_release["SupportScripts"] = {
+            'title': "SpiNNaker Support Scripts",
+            'module_path': "",
+            'citation_file_path': ""}
+
+
