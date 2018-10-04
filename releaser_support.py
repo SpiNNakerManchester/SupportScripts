57c06448090741e4baabdb6b82188e01060c91ce
alan-stokes <alan.barry.stokes@gmail.com>
2018-10-04 15:03:44 +0100

    ready to test with version updator

--- releaser_support.py
+++ releaser_support.py
@@ -1,12 +1,13 @@
 import os
-
-# modules
 import spinn_utilities
 import spinnman
-import spinnaker_graph_front_end
-import spynnaker8
-import spynnaker7
-import spynnaker
+from spinn_front_end_common.utilities import globals_variables
+import spinnaker_graph_front_end._version as gfe_version
+globals_variables.unset_simulator()
+globals_variables._failed_state = None
+import spynnaker8._version as pynn8_version
+import spynnaker7._version as pynn7_version
+import spynnaker._version as spynnaker_version
 import spinn_front_end_common
 import spinn_storage_handlers
 import spinn_machine
@@ -16,14 +17,67 @@ import spalloc
 import spalloc_server
 import pacman
 import data_specification
-
-
 from spinn_utilities.citation.tool_citation_generation import CITATION_FILE, \
     CitationAggregator
 from spinn_utilities.citation.tool_citation_generation import \
     CitationUpdaterAndDoiGenerator
+from version_updater import update_version_file, LANGUAGE_CODES
 
+# FIX THIS
 SPINN_COMMON_PATH = "/c/Users/alan/Documents/software/spinn_common"
+
+# UPDATE THIS
+NEW_VERSION_NAME = "Determinist"
+
+# SWITCH THIS WHEN READY
+PUBLISH_DOI = False
+
+# INSERT YOUR OWN ZENODO ACCESS TOKEN HERE, got from
+# https://zenodo.org/account/settings/applications/tokens/new/
+ZENODO_ACCESS_TOKEN = ""
+
+# UPDATE THIS FOR SECOND USAGE
+PREVIOUS_DOIS = {
+    pynn8_version: "https://zenodo.org/record/1255864#.W7YHW2hKiUk",
+    pynn7_version: "https://zenodo.org/record/1255864#.W7YHW2hKiUk"
+}
+
+# UPDATE IF NEW MODULES EXIST
+DOI_TITLES = {
+    spinn_utilities: "SpiNNaker Utilities",
+    spinnman: "Python Host to SpiNNaker Machines interface",
+    gfe_version: "Python Host top level API for SpiNNaker toolchain",
+    pynn8_version: "PyNN (0.8/0.9) on SpiNNaker API",
+    pynn7_version: "PyNN (0.75) on SpiNNaker API",
+    spynnaker_version: "Common functionality for PyNN on SpiNNaker API",
+    spinn_front_end_common: "Common functionality for SpiNNaker Front Ends",
+    spinn_storage_handlers: "General SpiNNaker File writers / readers",
+    spinn_machine: "Pythonic representation of a SpiNNaker Machine",
+    nengo_spinnaker_gfe: " Nengo on SpiNNaker API",
+    python_models8: "New model template for PyNN (0.8/ 0.9) on SpiNNaker",
+    spalloc: "SpiNNaker machine allocation client",
+    spalloc_server: "SpiNNaker machine allocation server",
+    pacman: "SpiNNaker mapping alogirthms",
+    data_specification: "SpiNNaker Data compression Language"
+}
+
+# UPDATE IF NEW MODULES EXIST
+PYTHON_MODULES = [
+    spinn_utilities,
+    spinnman,
+    gfe_version,
+    pynn8_version,
+    pynn7_version,
+    spynnaker_version,
+    spinn_front_end_common,
+    spinn_storage_handlers,
+    spinn_machine,
+    nengo_spinnaker_gfe,
+    python_models8,
+    spalloc,
+    spalloc_server,
+    pacman,
+    data_specification]
 
 
 class ReleaserSupport(object):
@@ -31,16 +85,37 @@ class ReleaserSupport(object):
     def __init__(self):
         self._modules_to_release = dict()
 
-        self._update_modules_to_release()
-        self._update_modules_for_updating_version_number()
+        self._setup_paths_input_data()
+        self._set_up_version_upgrade_points_for_release()
 
         for module in self._modules_to_release:
-            if module["major"] or module["minor"] or module["patch"]:
-                VersionUpdator()
-                CitationUpdaterAndDoiGenerator()
+            if (not self._modules_to_release[module]["major"] or
+                    self._modules_to_release[module]["minor"] or
+                    self._modules_to_release[module]["patch"]):
+                version_number, version_month, version_year, version_day = \
+                    update_version_file(
+                        self._modules_to_release[module]["language"],
+                        self._modules_to_release[module]["version_path"],
+                        self._modules_to_release[module]["major"],
+                        self._modules_to_release[module]["minor"],
+                        self._modules_to_release[module]["patch"],
+                        NEW_VERSION_NAME)
+                citation_updater = CitationUpdaterAndDoiGenerator()
+                citation_updater.update_citation_file_and_create_doi(
+                    self._modules_to_release[module]["citation_file_path"],
+                    update_version=True,
+                    version_number=version_number, version_month=version_month,
+                    version_year=version_year, version_day=version_day,
+                    doi_title=(self._modules_to_release[module]["doi_title"]
+                               + version_number),
+                    create_doi=True, publish_doi=PUBLISH_DOI,
+                    previous_doi=(
+                        self._modules_to_release[module]["previous_doi"]),
+                    is_previous_doi_sibling=False,
+                    zenodo_access_token=ZENODO_ACCESS_TOKEN,
+                    module_path=self._modules_to_release[module]["module_path"])
 
-
-    def _update_modules_for_updating_version_number(self):
+    def _set_up_version_upgrade_points_for_release(self):
         self._modules_to_release[spinn_utilities]["major"] = False
         self._modules_to_release[spinn_utilities]["minor"] = False
         self._modules_to_release[spinn_utilities]["patch"] = False
@@ -49,21 +124,21 @@ class ReleaserSupport(object):
         self._modules_to_release[spinnman]["minor"] = False
         self._modules_to_release[spinnman]["patch"] = False
 
-        self._modules_to_release[spinnaker_graph_front_end]["major"] = False
-        self._modules_to_release[spinnaker_graph_front_end]["minor"] = False
-        self._modules_to_release[spinnaker_graph_front_end]["patch"] = False
+        self._modules_to_release[gfe_version]["major"] = False
+        self._modules_to_release[gfe_version]["minor"] = False
+        self._modules_to_release[gfe_version]["patch"] = False
 
-        self._modules_to_release[spynnaker8]["major"] = False
-        self._modules_to_release[spynnaker8]["minor"] = False
-        self._modules_to_release[spynnaker8]["patch"] = False
+        self._modules_to_release[pynn8_version]["major"] = False
+        self._modules_to_release[pynn8_version]["minor"] = False
+        self._modules_to_release[pynn8_version]["patch"] = False
 
-        self._modules_to_release[spynnaker7]["major"] = False
-        self._modules_to_release[spynnaker7]["minor"] = False
-        self._modules_to_release[spynnaker7]["patch"] = False
+        self._modules_to_release[pynn7_version]["major"] = False
+        self._modules_to_release[pynn7_version]["minor"] = False
+        self._modules_to_release[pynn7_version]["patch"] = False
 
-        self._modules_to_release[spynnaker]["major"] = False
-        self._modules_to_release[spynnaker]["minor"] = False
-        self._modules_to_release[spynnaker]["patch"] = False
+        self._modules_to_release[spynnaker_version]["major"] = False
+        self._modules_to_release[spynnaker_version]["minor"] = False
+        self._modules_to_release[spynnaker_version]["patch"] = False
 
         self._modules_to_release[spinn_front_end_common]["major"] = False
         self._modules_to_release[spinn_front_end_common]["minor"] = False
@@ -109,28 +184,49 @@ class ReleaserSupport(object):
         self._modules_to_release["spinn_common"]["minor"] = False
         self._modules_to_release["spinn_common"]["patch"] = False
 
-    def _update_modules_to_release(self):
-        python_modules = [
-            spinn_utilities, spinnman, spinnaker_graph_front_end,
-            spynnaker8, spynnaker7, spynnaker, spinn_front_end_common,
-            spinn_storage_handlers, spinn_machine,
-            nengo_spinnaker_gfe, python_models8, spalloc,
-            spalloc_server, pacman, data_specification]
-        for python_module in python_modules:
+    def _setup_paths_input_data(self):
+        for python_module in PYTHON_MODULES:
             self._modules_to_release[python_module] = {
-                'module_path': os.path.dirname(os.path.abspath(
-                    spinn_utilities.__file__)),
-                'citation_file_path': os.path.join(os.path.dirname(
-                    os.path.abspath(python_module.__file__)), CITATION_FILE)}
+                'module_path': os.path.dirname(os.path.dirname(os.path.abspath(
+                    python_module.__file__))),
+                'version_path': os.path.join(os.path.dirname(os.path.abspath(
+                    python_module.__file__)), "_version.py"),
+                'citation_file_path': os.path.join(
+                    os.path.dirname(os.path.dirname(os.path.abspath(
+                        python_module.__file__))), CITATION_FILE),
+                'language': LANGUAGE_CODES.PYTHON}
+            if python_module in PREVIOUS_DOIS:
+                self._modules_to_release[python_module]["previous_doi"] = \
+                    PREVIOUS_DOIS[python_module]
+            else:
+                self._modules_to_release[python_module]["previous_doi"] = None
 
         self._modules_to_release["spinnaker_tools"] = {
             'module_path':
                 CitationAggregator.locate_path_for_c_dependency(
                     "spinnaker_tools"),
+            'version_path': os.path.join(
+                CitationAggregator.locate_path_for_c_dependency(
+                    "spinnaker_tools"), "_version.py"),
             'citation_file_path': os.path.join(
-                self._modules_to_release["spinnaker_tools"]['module_path'],
-                CITATION_FILE)}
+                CitationAggregator.locate_path_for_c_dependency(
+                    "spinnaker_tools"), CITATION_FILE),
+            'language': LANGUAGE_CODES.C}
+        if "spinnaker_tools" in PREVIOUS_DOIS:
+            self._modules_to_release["spinnaker_tools"]["previous_doi"] = \
+                PREVIOUS_DOIS["spinnaker_tools"]
+        else:
+            self._modules_to_release["spinnaker_tools"]["previous_doi"] = None
+
         self._modules_to_release["spinn_common"] = {
             'module_path': SPINN_COMMON_PATH,
             'citation_file_path': os.path.join(
-                SPINN_COMMON_PATH, CITATION_FILE)}
+                SPINN_COMMON_PATH, CITATION_FILE),
+            'language': LANGUAGE_CODES.C}
+        if "spinn_common" in PREVIOUS_DOIS:
+            self._modules_to_release["spinn_common"]["previous_doi"] = \
+                PREVIOUS_DOIS["spinn_common"]
+        else:
+            self._modules_to_release["spinn_common"]["previous_doi"] = None
+
+x = ReleaserSupport()
---
+++ version_updater.py
@@ -0,0 +1,131 @@
+from datetime import date
+import datetime
+
+from enum import Enum
+
+VERSION_FILE = "_version.py"
+
+# c magic params
+C_VERSION_NUM = "SLLT_VER_STR"
+C_VERSION_NUM_HEX = "SLLT_VER_NUM"
+C_VERSION_MONTH = "SLLT_VER_MON"
+C_VERSION_DAY = "SLLT_VER_DAY"
+C_VERSION_YEAR = "SLLT_VER_YEAR"
+C_VERSION_NAME = "SLLT_VER_NAME"
+C_HASH_DEFINE = "#define"
+
+# python magic params
+PYTHON_VERSION_NUM = "__version__ = "
+PYTHON_VERSION_MONTH = "__version_month__ = "
+PYTHON_VERSION_YEAR = "__version_year__ = "
+PYTHON_VERSION_DAY = "__version_day__ = "
+PYTHON_VERSION_NAME = "__version_name__ = "
+
+VERSION_EPOCH = "1!"
+
+LANGUAGE_CODES = Enum(
+    value="LANGUAGE_CODES",
+    names=[
+        ("PYTHON", 0),
+        ("C", 1)])
+
+
+def update_version_file(language, version_path, major, minor, patch, name):
+    today = str(date.today())
+    date_bits = today.split("-")
+    month_text = datetime.datetime.now().strftime("%B")
+
+    version_number = None
+    version_year = date_bits[0]
+    version_day = date_bits[2]
+    version_month = month_text
+
+    version_file = open(version_path, "r")
+    in_lines = version_file.readlines()
+    version_file.close()
+
+    output_file = open(version_path, "w")
+
+    for in_line in in_lines:
+        if language == LANGUAGE_CODES.PYTHON:
+            if len(in_line.split(PYTHON_VERSION_DAY)) != 1:
+                output_file.write(PYTHON_VERSION_DAY + "\"{}\"".format(
+                    version_day) + "\n")
+            elif len(in_line.split(PYTHON_VERSION_MONTH)) != 1:
+                output_file.write(PYTHON_VERSION_MONTH + "\"{}\"".format(
+                    month_text) + "\n")
+            elif len(in_line.split(PYTHON_VERSION_NAME)) != 1:
+                output_file.write(PYTHON_VERSION_NAME + "\"{}\"".format(name)
+                                  + "\n")
+            elif len(in_line.split(PYTHON_VERSION_YEAR)) != 1:
+                output_file.write(PYTHON_VERSION_YEAR + "\"{}\"".format(
+                    version_year) + "\n")
+            elif len(in_line.split(PYTHON_VERSION_NUM)) != 1:
+                version_number = _update_version_number(
+                    in_line.split(PYTHON_VERSION_NUM)[1], major, minor, patch)
+                output_file.write(PYTHON_VERSION_NUM + "\"{}\"".format(
+                    version_number) + "\n")
+            else:
+                output_file.write(in_line)
+        elif language == LANGUAGE_CODES.C:
+            if len(in_line.split(C_VERSION_DAY)) != 1:
+                output_file.write(C_HASH_DEFINE + C_VERSION_DAY + " " +
+                                  "\"{}\"".format(version_day) + "\n")
+            elif len(in_line.split(C_VERSION_MONTH)) != 1:
+                output_file.write(C_HASH_DEFINE + C_VERSION_MONTH + " " +
+                                  "\"{}\"".format(version_month) + "\n")
+            elif len(in_line.split(C_VERSION_NAME)) != 1:
+                output_file.write(C_HASH_DEFINE + C_VERSION_NAME + " " +
+                                  "\"{}\"".format(name) + "\n")
+            elif len(in_line.split(C_VERSION_YEAR)) != 1:
+                output_file.write(C_HASH_DEFINE + C_VERSION_YEAR + " " +
+                                  "\"{}\"".format(version_year) + "\n")
+            elif len(in_line.split(C_VERSION_NUM)) != 1:
+                version_number = _update_version_number(
+                    in_line.split(C_VERSION_NUM)[1], major, minor, patch)
+                output_file.write(C_HASH_DEFINE + C_VERSION_NUM + " " +
+                                  "\"{}\"".format(version_number) + "\n")
+            elif len(in_line.split(C_VERSION_NUM_HEX)) != 1:
+                version_bits = version_number.split(".")
+                output_file.write(
+                    C_HASH_DEFINE + C_VERSION_NUM_HEX + " " + "0x0" +
+                    version_bits[0] + "0" + version_bits[1] + "0" +
+                    version_bits[2] + "\n")
+            else:
+                output_file.write(in_line)
+        else:
+            raise Exception("WTF")
+    output_file.flush()
+    output_file.close()
+    return version_number, version_month, version_year, version_day
+
+
+def _update_version_number(version_number, major, minor, patch):
+    version_num_bits = version_number.split(".")
+    previous_patch = int(version_num_bits[2])
+    previous_minor = int(version_num_bits[1])
+
+    saw_epoch = False
+    if len(version_num_bits[0].split(VERSION_EPOCH)) != 1:
+        previous_major = int(version_num_bits[0].split("!")[1])
+        saw_epoch = True
+    else:
+        previous_major = int(version_num_bits[0])
+
+    if patch:
+        previous_patch += 1
+    if minor:
+        previous_minor += 1
+    if major:
+        previous_major += 1
+
+    new_version_num = (
+        str(previous_major) + "." + str(previous_minor) + "." +
+        str(previous_patch))
+    if saw_epoch:
+        new_version_num = VERSION_EPOCH + new_version_num
+    return new_version_num
+
+
+
+
