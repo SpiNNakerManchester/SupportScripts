9a73ebdac458d85c2c3f4328f69f80757bb17641
Donal K. Fellows <donal.k.fellows@manchester.ac.uk>
2023-04-11 14:04:21 +0100

    More tweaking

---
+++ actions/pylint/run_pylint.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copyright (c) 2020 The University of Manchester
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if test -n "$SPELL_LANG"; then
+	pylint --output-format=colorized "--disable=$DISABLE_CATS" \
+		--persistent=no "--jobs=$JOBS" --rcfile=$RC \
+		"--spelling-dict=$SPELL_LANG" --spelling-private-dict-file=/tmp/dict.txt \
+		$PACKAGES
+else
+	pylint --output-format=colorized "--disable=$DISABLE_CATS" \
+		--persistent=no "--jobs=$JOBS" "--rcfile=$RC" \
+		$PACKAGES
+fi
+# Note that there's special conditioning of the return code of pylint
+exit $(($? & $FAIL_CODE))
