1bd0f979ff2f2a922d9d2cb8c517b6b04e6e3661
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2024-05-29 14:24:56 +0100

    revert recent changes

--- actions/configure-spynnaker/mkcfg.bash
+++ actions/configure-spynnaker/mkcfg.bash
@@ -14,33 +14,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+cfg=~/.spynnaker.cfg
 
-echo '[Machine]' > $CFG_FILE
+echo '[Machine]' > $cfg
 
 if [ "x$V_TYPE" = "xtrue" ]; then
-	echo "virtual_board = True" >> $CFG_FILE
-	echo "width = $V_WIDTH" >> $CFG_FILE
-	echo "height = $V_HEIGHT" >> $CFG_FILE
+	echo "virtual_board = True" >> $cfg
+	echo "width = $V_WIDTH" >> $cfg
+	echo "height = $V_HEIGHT" >> $cfg
 elif [ "x$M_ADDR}" = "x" ]; then
-	echo "spalloc_server = $S_HOST" >> $CFG_FILE
-	echo "spalloc_port = $S_PORT" >> $CFG_FILE
-	echo "spalloc_user = $S_USER" >> $CFG_FILE
+	echo "spalloc_server = $S_HOST" >> $cfg
+	echo "spalloc_port = $S_PORT" >> $cfg
+	echo "spalloc_user = $S_USER" >> $cfg
 else
-	echo "machineName = $M_ADDR" >> $CFG_FILE
-	echo "version = $M_VERSION" >> $CFG_FILE
+	echo "machineName = $M_ADDR" >> $cfg
+	echo "version = $M_VERSION" >> $cfg
 fi
 
-echo "time_scale_factor = $M_TSF" >> $CFG_FILE
+echo "time_scale_factor = $M_TSF" >> $cfg
 
-echo '[Database]' >> $CFG_FILE
+echo '[Database]' >> $cfg
 
-echo '[Simulation]' >> $CFG_FILE
+echo '[Simulation]' >> $cfg
 
-echo '[Buffers]' >> $CFG_FILE
+echo '[Buffers]' >> $cfg
 
-echo '[Mode]' >> $CFG_FILE
-echo "mode = $M_MODE" >> $CFG_FILE
+echo '[Mode]' >> $cfg
+echo "mode = $M_MODE" >> $cfg
 
-echo '[Reports]' >> $CFG_FILE
-echo "default_report_file_path = $F_PATH" >> $CFG_FILE
-echo "default_application_data_file_path = $F_PATH" >> $CFG_FILE
+echo '[Reports]' >> $cfg
+echo "default_report_file_path = $F_PATH" >> $cfg
+echo "default_application_data_file_path = $F_PATH" >> $cfg
